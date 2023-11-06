4175008673bc8203ac2100380fa2f60e12986b39
Cristina Calvo <xmcaldia327@ieshnosmachado.org>
2023-11-06 11:18:12 +0100

    Proyecto Task terminado :)

--- task/views.py
+++ task/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Task
 
-# Create your views here.
+def task_list(request):
+    tasks = Task.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'task/task_list.html', {'tasks': tasks})
