cb912084bf5803d93212432e3a5dfedcbf0093d0
Cristina Calvo <xmcaldia327@ieshnosmachado.org>
2023-11-15 10:50:58 +0100

    Formulario y detalles

--- task/urls.py
+++ task/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.task_list, name='task_list'),
+	#path('', views.task_list, name='task_list'),
+	#path('nueva_task/', views.nueva_task, name='nueva_task'),
+	path('', views.TaskClass.as_view(), name='task_list'),
+	path('nueva_task/', views.TaskNueva.as_view(), name='nueva_task'),
+    path('detalles/<int:pk>/', views.DetallesClass.as_view(), name='detalles')
 ]
--- task/views.py
+++ task/views.py
@@ -1,7 +1,72 @@
-from django.shortcuts import render
-from django.utils import timezone
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
+from django.views import View
+from django.shortcuts import get_object_or_404
 
+"""def task_list(request):
+    tasks = Task.objects.all()
+
+    if request.method == 'POST':
+        form=TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form=TaskForm()
+    return render(request, 'task/task_list.html', {'tasks': tasks, 'form' : form})"""
+
+"""
 def task_list(request):
-    tasks = Task.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'task/task_list.html', {'tasks': tasks})
+ tasks = Task.objects.all()
+ return render(request, 'task/task_list.html', {'tasks': tasks})
+
+
+def nueva_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('nueva_task') 
+    else:
+        form = TaskForm()
+
+    return render(request, 'task/nueva_tarea.html', {'form': form})"""
+
+
+class TaskClass(View):
+    template_name = 'task/task_list.html'
+
+    def get(self, request):
+        tasks = Task.objects.all()
+        return render(request, self.template_name, {'tasks': tasks})
+
+    def post(self, request):
+        form = TaskForm(request.POST)
+        form.save()
+        tasks = Task.objects.all() #actualizar
+        return render(request, self.template_name, {'tasks': tasks})
+        
+
+class TaskNueva(View):
+    tasks = Task.objects.all()
+
+    def post(self, request):
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('nueva_task')
+        return render(request, 'task/nueva_tarea.html', {'tasks': self.tasks, 'form' : form})
+    
+
+    def get(self, request):
+        tasks = Task.objects.all()
+        form=TaskForm()
+        return render(request, 'task/nueva_tarea.html', {'tasks': tasks, 'form' : form})
+                
+
+class DetallesClass(View):
+
+    def get(self, request, pk): 
+     task = get_object_or_404(Task, pk=pk) #no queremos todas las task solo la indicada, pasamos pk 
+     return render(request, 'task/detalles.html', {'tasks': task})
